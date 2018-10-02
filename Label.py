e453f7a7ddd198a2820624b6cda5f173b16df2ad
Demitri Maestas <chucho2@unm.edu>
2018-10-02 13:04:10 -0600

    Working MLE

---
+++ Label.py
@@ -0,0 +1,14 @@
+
+
+
+class Label():
+  
+  label = 0;
+  MLE = 0;
+
+  def __init__(self, Yk, MLE ,dataSet):
+    self.label = Yk
+    self.MLE = MLE
+    self.dataSet = dataSet;
+   
+        
--- Naive.py
+++ Naive.py
@@ -1,13 +1,26 @@
 from collections import Counter
 from IO import CacheIn, CacheOut
+from Label import Label
 
 
 class NaiveBayes():
-    docs = 61189
-    Y = dict()
+    docsCol = 61189
+    labels = list()
+
+
     def __init__(self, trainingData, testingData, predictionData, vocab):
         self.trainingData = trainingData
         self.testingData = testingData
         self.predictionData = predictionData
-        Y = Counter(trainingData.getcol(self.docs).data)
-        #print(Y)
+        self.load_labels()
+       
+
+    def load_labels(self):
+      Y = Counter(self.trainingData.getcol(self.docsCol).data)
+      total = len(self.trainingData.getcol(self.docsCol).data)
+      for k in Y:
+        MLE = Y.get(k)/total
+        print(""+str(MLE) +" "+ str(Y.get(k))+" "+str(total))
+        self.labels.append(Label(k,MLE,self.trainingData))
+
+   
